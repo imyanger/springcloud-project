2cd9f41634b10c8062355fbe4ecb7e09cfa916bc
github-yanger <550799932@qq.com>
2019-01-20 18:51:22 +0800

    config

---
+++ p6-config/user-server/src/main/java/com/yanger/UserServerApplication.java
@@ -0,0 +1,33 @@
+package com.yanger;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestTemplate;
+
+@EnableHystrix // 开启hystrix功能
+@EnableFeignClients // @EnableFeignClients注解，用来为应用开启Feign相关功能
+@EnableEurekaClient
+@SpringBootApplication // 声明一个springboot项目
+@ComponentScan("com.yanger.*") // 扫描注解的路径，可不配置，不配置则默认扫描启动类同包下的文件
+@MapperScan("com.yanger.*.dao") // 引入项目中mybatis的mapper类
+public class UserServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServerApplication.class, args);
+	}
+	
+	@Bean
+    @LoadBalanced
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+}
+
